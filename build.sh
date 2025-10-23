f9ac79ef7278e19ce4ab55f4ff8e4fc7d7a49fe0
元プログラマi <99943229+moto-programmer-i@users.noreply.github.com>
2025-10-24 01:02:40 +0900

    vulkanライブラリが不要だった

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ cd out
 
 # 実行ファイル作成
 # libglfw.so をリンクしたいときは -lglfw （先頭のlibと、拡張子は不要）
-g++ -std=c++23 -fmodules -fPIC ../main.cpp -lglfw -lvulkan -L. Glfw.so VulkanTutorial.so -o main
+g++ -std=c++23 -fmodules -fPIC ../main.cpp -lglfw -L. Glfw.so VulkanTutorial.so -o main
 
